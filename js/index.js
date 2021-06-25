731a06258ebb3a71b45a7768d013863d7f35f9cb
thinhxd12 <thinhxd12@gmail.com>
2021-06-25 23:20:10 +0700

    fix_header

--- js/index.js
+++ js/index.js
@@ -4,11 +4,14 @@ $(document).ready(function () {
         var position = $(this).scrollTop();
         if (position >= 300) {
             $('.header').addClass('custom-header');
-            $('.header').removeClass('top-header');
+            $('.header').removeClass('top-header hide-header');
         }
-        else {
+        else if (position < 300 && position >= 100) {
+            $('.header').addClass('hide-header');
+        }
+        else if (position < 100) {
             $('.header').addClass('top-header');
-            $('.header').removeClass('custom-header');
+            $('.header').removeClass('custom-header hide-header');
         }
     });
 });
