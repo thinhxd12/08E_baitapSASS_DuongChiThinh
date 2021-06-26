3614dba812551c31bd16ceea1d9781107278dd04
thinhxd12 <thinhxd12@gmail.com>
2021-06-26 15:09:42 +0700

    done

--- js/index.js
+++ js/index.js
@@ -2,16 +2,19 @@
 $(document).ready(function () {
     $(window).scroll(function () {
         var position = $(this).scrollTop();
-        if (position >= 300) {
-            $('.header').addClass('custom-header');
-            $('.header').removeClass('top-header hide-header');
-        }
-        else if (position < 300 && position >= 100) {
-            $('.header').addClass('hide-header');
-        }
-        else if (position < 100) {
-            $('.header').addClass('top-header');
-            $('.header').removeClass('custom-header hide-header');
+        var width = $(window).width();
+        if (width > 576) {
+            if (position >= 300) {
+                $('.header').addClass('custom-header');
+                $('.header').removeClass('top-header hide-header');
+            }
+            else if (position < 300 && position >= 100) {
+                $('.header').addClass('hide-header');
+            }
+            else if (position < 100) {
+                $('.header').addClass('top-header');
+                $('.header').removeClass('custom-header hide-header');
+            }
         }
     });
 });
